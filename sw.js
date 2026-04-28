5114fc9e2223a748236e46982a4e0c8d26b82306
Ahmad <ahmad@example.com>
2026-04-28 22:32:02 +0700

    V4.7: Final PWA Installation Fix

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install', (e) => { e.waitUntil(caches.open('v1').then((cache) => cache.addAll(['index.html', 'icon.jpg']))); }); self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request))); });
